dfcbeb85cdf725e97882424a6134d785a7d5c940
Camilla Souto <104527029+Camis-souto@users.noreply.github.com>
2022-10-13 11:57:42 -0300

    Deply

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,54 @@
+import { Typography } from '@mui/material';
 import React from 'react'
+import Link from 'next/link';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
-export default function login() {
+
+type CopyProps ={
+  site: string;
+  sx?:object;
+}
+
+function Copyright(props:CopyProps){
   return (
-    <div>login</div>
+    <Typography>
+      {'Copyright ©️'}
+      <Link color='inherit' href='https://www.avanade.com.br/'>
+        {props.site}
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  )
+}
+const theme = createTheme();
+
+export default function LoginPage() {
+  return (
+<ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Box sx={{mt:8, display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+              <Typography component="h1" variant="h5">
+                Login
+              </Typography>
+            <Box component="form" onSubmit={(e)=>{console.log(`enviou`)}}>
+          <TextField required fullWidth id="email" label="Digite o e-mail" name="email" autoComplete="email" autoFocus/>
+          <TextField required fullWidth id="password" type="password" label="Digite a senha" name="password" autoComplete="current-password" autoFocus/>
+            </Box>
+
+
+
+
+            </Box>
+            <div>login</div>
+            <Copyright site="www.avanade.com.br" sx={{mt:8, mb: 4}} />
+        </Container>
+    </ThemeProvider>
+
   )
 }
